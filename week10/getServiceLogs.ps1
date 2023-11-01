9019804327831aeabd92b1b7fc0bcc9e2ff47e77
Espen-ChamplainCollege <espen.wold@mymail.champlain.edu>
2023-11-01 13:16:39 -0700

    Finished get service logs assignment

---
+++ week10/getServiceLogs.ps1
@@ -0,0 +1,19 @@
+﻿$possibleInputs = @('all', 'stopped', 'running')
+
+$input = ""
+
+while ($input -ine 'quit'){
+    if ($possibleInputs -icontains $input){
+        if ($input -ieq $possibleInputs[0]){
+            Get-Service
+        }
+        if ($input -ieq $possibleInputs[1]) {
+            Get-Service | Where-Object { $_.Status -ilike $possibleInputs[1] }
+        }
+        if ($input -ieq $possibleInputs[2]) {
+            Get-Service | Where-Object { $_.Status -ilike $possibleInputs[2] }
+        }
+    }
+
+    $input = Read-Host -Prompt "Type 'all', 'stopped', or 'running' to view respective services. Type 'quit' to stop"
+}
